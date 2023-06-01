f4a6c377c26064c2a929752041377345cad520bd
WillemVanM <92741161+WillemVanM@users.noreply.github.com>
2023-06-01 11:21:10 +0200

    Update data_retrieve.py

--- lens/Archive/data_retrieve.py
+++ lens/Archive/data_retrieve.py
@@ -1,6 +1,1 @@
-from mp_api.client import MPRester
 
-with MPRester(api_key='<enter your api key>') as mpr:
-    bandstructure = mpr.get_bandstructure_by_material_id('mp-30')
-
-print(bandstructure)
